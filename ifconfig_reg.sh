0237ba5639f39c095f491812d9eafb74b8733edd
ptallrights <ptallrights@163.com>
2016-08-22 17:54:01 +0800

    modify a little

--- ifconfig_reg.sh
+++ ifconfig_reg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-NET_NAME=`ifconfig |grep -v "^ "|grep " "|awk -F: '{print $1}'`
-NET_IP=`ifconfig |grep "inet"|grep -v "inet6"|awk -F " " '{print $2}'`
+NET_NAME=`ifconfig |grep -v "^ "|grep " "|awk -F " " '{print $1}'`
+NET_IP=`ifconfig |grep "inet addr"|awk -F: '{print $2}'|awk -F " " '{print $1}'`
 ifconfig |grep -v "^ "|grep " "|awk -F: '{print $1}' >/tmp/ifconfig_bk
 NUMBER=`wc -l /tmp/ifconfig_bk |awk -F " " '{print $1}'`
 number=`echo "$NUMBER-1"|bc`
@@ -11,3 +11,4 @@ echo $net
 for i in $(seq 0 $number);do
 echo "${net_name[i]}:${net_ip[i]}"
 done
+~    
